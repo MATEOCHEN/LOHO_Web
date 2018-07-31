f325694c17191b2ba157302ce47213a568a0a550
KevinYuntech <row123321@gmail.com>
2018-07-31 12:05:41 +0800

    別名找不到function

--- LOHO_Web/routes/web.php
+++ LOHO_Web/routes/web.php
@@ -15,9 +15,16 @@ Route::get('/','HomeController@Index');
 
 //Route::get('/info','HomeController@info');
 
-Route::get('Shopping/BrowseItems','Shopping\ShoppingController@BrowseItems');
+//Route::get('Shopping/BrowseItems','Shopping\ShoppingController@BrowseItems');
 
-Route::get('Shopping/ShoppingItem','Shopping\ShoppingController@ShoppingItem');
+//Route::get('Shopping/ShoppingItem','Shopping\ShoppingController@ShoppingItem');
+/*
+Route::group(['prefix'=>'Shopping'],function(){
+    Route::get('BrowseItems','Shopping\ShoppingController@BrowseItems');
+    Route::get('ShoppingItem','Shopping\ShoppingController@ShoppingItem');
+});*/
+
+Route::get('Shopping/BrowseItems', array('as' => 'BrowseItems', 'uses' => 'ShoppingController@BrowseItems'));
 
 Route::get('/student/{student_no}', function ($student_no) {
     return "學號:".$student_no;
