39f26fe9f2a06894c03d27da91bc2f9029d8a5bb
KevinYuntech <row123321@gmail.com>
2018-08-27 21:16:54 +0800

    解決第一項商品無法加入購物車bug(if [])

---
+++ LOHO_Web/app/Http/Controllers/Shopping/Cart_Imp.php
@@ -0,0 +1,74 @@
+<?php
+namespace App\Http\Controllers\Shopping;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class Cart_Imp
+{
+
+    public function addCart(Request $request)
+    {   
+        $cartItems = Session::get('cart.item', []);
+        $current_cartItem;
+        if($cartItems == []){
+            $item = [
+                'id' => $request->item_id,
+               'name' => $request->item_name,
+               'price' => $request->item_price,
+               'count' => $request->item_count
+             ];
+             $current_cartItem =  $item;
+             $request->session()->push('cart.item', $item);
+        }
+        else{
+            foreach ($cartItems as &$cartItem) {
+                if ($cartItem['name'] == $request->item_name) {
+                        $cartItem['count']  += $request->item_count;
+                        $current_cartItem = $cartItem;
+                        $request->session()->put('cart.item', $cartItems);
+                        break;
+                }
+                else{
+                    $item = [
+                        'id' => $request->item_id,
+                       'name' => $request->item_name,
+                       'price' => $request->item_price,
+                       'count' => $request->item_count
+                     ];
+                     $current_cartItem =  $item;
+                     $request->session()->push('cart.item', $item);
+                }
+            }
+        }
+
+
+        return response()->json(array('item'=>$current_cartItem));
+    }
+
+    public function getCart()
+    {
+        $cart = Session::get('cart');
+        $items = $cart['item'];
+
+        return response()->json(['items' => $items]);
+    }
+
+    public function updateCart(Request $request)
+    {   //須研究foreach及session::put
+        $cartItems = Session::get('cart.item', []);
+        $current_cartItem;
+        foreach ($cartItems as &$cartItem) {
+            if ($cartItem['name'] == $request->item_name) {
+                    $cartItem['count']  = $request->item_count;
+                    $current_cartItem = $cartItem;
+                    break;
+            }
+        }
+        $request->session()->put('cart.item', $cartItems);
+        return response()->json(['item' => $current_cartItem]);
+    }
+}
--- LOHO_Web/app/Http/Controllers/Shopping/ShoppingController.php
+++ LOHO_Web/app/Http/Controllers/Shopping/ShoppingController.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+
+use  App\Http\Controllers\Shopping\Cart_Imp;
 class ShoppingController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
@@ -18,27 +20,6 @@ class ShoppingController extends BaseController
 
     public function ShoppingItem(){
         return view('Shopping\ShoppingItem');
-    }
-
-    public function addCart(Request $request)
-    {   //需增加相同商品判別
-        $item = [
-             'id' => $request->item_id,
-            'name' => $request->item_name,
-            'price' => $request->item_price,
-            'count' => $request->item_count
-          ];
-          $request->session()->push('cart.item', $item);
-        
-        return response()->json(array('item'=>$item));
-    }
-
-    public function getCart()
-    {
-        $cart = Session::get('cart');
-        $items = $cart['item'];
-
-        return response()->json(['items' => $items]);
     }
 
     public function ShoppingCart()
@@ -60,18 +41,21 @@ class ShoppingController extends BaseController
         return view('ShoppingCart\ShoppingCart',compact('item_data'));
     }
 
+    public function addCart(Request $request)
+    {   
+        $cart_imp = new Cart_Imp();
+        return $cart_imp->addCart($request);
+    }
+
+    public function getCart()
+    {
+        $cart_imp = new Cart_Imp();
+        return $cart_imp->getCart();
+    }
+
     public function updateCart(Request $request)
     {   //須研究foreach及session::put
-        $cartItems = Session::get('cart.item', []);
-        $current_cartItem;
-        foreach ($cartItems as &$cartItem) {
-            if ($cartItem['name'] == $request->item_name) {
-                    $cartItem['count']  = $request->item_count;
-                    $current_cartItem = $cartItem;
-                    break;
-            }
-        }
-        $request->session()->put('cart.item', $cartItems);
-        return response()->json(['item' => $current_cartItem]);
+        $cart_imp = new Cart_Imp();
+        return $cart_imp->updateCart($request);
     }
 }
