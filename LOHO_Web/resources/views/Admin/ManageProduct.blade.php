078b2f1094815e4d99ac2fd83eb9601df9a5bfc1
KevinYuntech <row123321@gmail.com>
2018-09-03 16:48:10 +0800

    jquery button可抓到前一個input資料 但上傳圖片也會一起被disable

--- LOHO_Web/resources/views/Admin/ManageProduct.blade.php
+++ LOHO_Web/resources/views/Admin/ManageProduct.blade.php
@@ -2,22 +2,50 @@
 
 @section('title','上傳商品')
 @section('head')
-<link href="" rel="stylesheet" type="text/css" />
-@stop
+<link href="{{ URL::asset('/css/ManageProduct.css') }}" rel="stylesheet" type="text/css" />
+<script>
+    $(document).ready(function () {
+        $(':input[type="submit"]').prop('disabled', true);
+        $(':input[type="text"]').keyup(function() {
+            if($(this).val() != '') {
+                let parent_dom = $(this).parent("li");
+                let text_dom = parent_dom.children(':input[type="submit"]');
+                text_dom.prop('disabled', false);
+            }
+        });
 
+        $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+        });
+        
+    $(':input[type="submit"]').click(function (e) { 
+        alert("click");
+        let parent_dom = $(this).parent("li");
+        let text_dom = parent_dom.children(':input[type="text"]');
+        // First way to get a value
+        value = text_dom.val(); 
+        alert(value);
+        
+    });
+
+    });
+
+</script>
+@stop
 
 @section('content')
 <div class = "content">
-
     <div class="d-flex flex-row justify-content-center">
-            <div>
-                <h5>目前商品(圖片大小限制為64KB, 格式限制為jpeg、png、bmp、gif、 或 svg)</h5>
-                <div>商品編號:{{$data['id']}}</div>
-                <div>商品名稱:{{$data['name']}}</div>
-                <div>商品編號:{{$data['price']}}</div>
-                <div>商品描述:{{$data['description']}}</div>
-                <div>剩餘數量:{{$data['remain_count']}}</div>
-                <img src="{{ asset($data['img_url']) }}" height="300" width="300"> 
+            <ul>
+                <li><h5>目前商品(圖片大小限制為64KB, 格式限制為jpeg、png、bmp、gif、 或 svg)</h5></li>
+                <li>商品編號:<input type="text" name="id" id="" value="{{$data['id']}}"><input type="submit" value="更改" class="btn btn-primary btn-sm modify"></li>
+                <li>商品名稱:<input type="text" name="name" id="" value="{{$data['name']}}"><input type="submit" value="更改" class="btn btn-primary btn-sm modify"></li>
+                <li>商品價錢:<input type="text" name="price" id="" value="{{$data['price']}}"><input type="submit" value="更改" class="btn btn-primary btn-sm modify"></li>
+                <li>商品描述:<input type="text" name="description" id="" value="{{$data['description']}}"><input type="submit" value="更改" class="btn btn-primary btn-sm modify"></li>
+                <li>剩餘數量:<input type="text" name="remain_count" id="" value="{{$data['remain_count']}}"><input type="submit" value="更改" class="btn btn-primary btn-sm modify"></li>
+                <li><img src="{{ asset($data['img_url']) }}" height="300" width="300"> 
                     <form method="post" enctype="multipart/form-data" action="/LOHO_Web/public/upload">
                         {{ csrf_field() }}
                         <input type="file" name="file" />
@@ -25,8 +53,8 @@
                         @foreach ($errors->all() as $error)
                             <ul><li>{{ $error }}</li></ul>  
                         @endforeach
-                    </form>
-            </div>
+                    </form></li>
+            </ul>
 </div>
 
 @stop
