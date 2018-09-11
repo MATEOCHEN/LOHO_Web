efe50c727e47c108e3ce8801e18cbb07c94a0c2e
KevinYuntech <row123321@gmail.com>
2018-09-11 19:34:48 +0800

    更改比對帳號, 與註冊帳號欄位

--- LOHO_Web/app/Http/Controllers/Account/AccountControllerImp/AfterRegisterAccount.php
+++ LOHO_Web/app/Http/Controllers/Account/AccountControllerImp/AfterRegisterAccount.php
@@ -2,19 +2,51 @@
 namespace App\Http\Controllers\Account\AccountControllerImp;
 use App\User;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Auth;
+use Validator;
 
 class AfterRegisterAccount implements AccountControllerPostImp
 {
     public function handle(\Illuminate\Http\Request $request)
-    {
-        $user = new User;
-        
-        $user->name = $request->account;
-        $password = bcrypt($request->password);
-        $user->password = $password;
-        $user->email = $request->email;
-        $user->save();
-        
-        echo '成功註冊';
+    {   
+        $input = $request->all();
+        $rules = [
+        'account' => 'required| between:4,20',
+        'password' => 'required| between:4,20|confirmed',
+        'email' => 'required| email',
+        ];
+
+        $messages = [
+        'account.required'=>'帳號必填欄位',
+        'account.between'=>'帳號必須4-20位數字',
+        'password.required'=>'密碼必填欄位',
+        'password.between'=>'密碼必須4-20位數字',
+        'password.confirmed'=>'欲修改密碼與確認密碼需一致',
+        'email.required'=>'email必填欄位',
+        'email.email'=>'email格式',
+        ];
+        $validator = Validator::make($input,$rules,$messages);
+
+        if($validator->passes())
+        {
+            $user = new User;
+            //要做驗證
+            $user->account = $request->account;
+            $user->password = bcrypt($request->password);
+            $user->name = $request->name;
+            $user->telephone_number = $request->telephone_number;
+            $user->phone_number = $request->phone_number;
+            $user->address = $request->address;
+            $user->email = $request->email;
+            //$user->is_subscribe = $request->is_subscribe;
+            $user->save();
+            Auth::login($user);
+            return redirect('/');
+        }
+        else
+        {
+            return back()->withErrors($validator);
+        }
+
     }
 }
