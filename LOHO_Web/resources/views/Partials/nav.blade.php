40c12d4bd238eb6c6ffbe0f04d806cf457e877da
Mateo <stezen129@gmail.com>
2018-11-14 17:13:06 +0800

    LOGO Changed

--- LOHO_Web/resources/views/Partials/nav.blade.php
+++ LOHO_Web/resources/views/Partials/nav.blade.php
@@ -4,7 +4,7 @@
 <div id = "nav">
     <nav class="navbar navbar-expand-lg navbar-light bg-light" >
         <a class="navbar-brand" href="{{ url("/") }}">
-            <img alt="Brand" src="<?php echo asset('/Image/LOGO.svg');?>" width="150px" height="70spx">
+            <img alt="Brand" src="<?php echo asset('/svg/New LOGO.svg');?>" width="150px" height="70spx">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
