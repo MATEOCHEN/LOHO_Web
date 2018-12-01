453f3eef2a9ae4e8e7a8601e3137691727f7f3e3
KevinYuntech <row123321@gmail.com>
2018-12-01 16:39:35 +0800

    1201 big bug

--- LOHO_Web/app/Users_own_voucher.php
+++ LOHO_Web/app/Users_own_voucher.php
@@ -8,4 +8,8 @@ class Users_own_voucher extends Model
 {
     //
     protected $table = 'users_own_vouchers';
+    protected $primaryKey = ['user_id', 'voucher_id'];
+    protected $fillable = [
+        'user_id','voucher_id','using_state'
+    ];
 }
