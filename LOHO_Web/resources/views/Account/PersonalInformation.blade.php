520e6aa1cd5cf0616f9923cdc7bad326080c924d
Mateo <stezen129@gmail.com>
2018-12-07 17:11:09 +0800

    account update

--- LOHO_Web/resources/views/Account/PersonalInformation.blade.php
+++ LOHO_Web/resources/views/Account/PersonalInformation.blade.php
@@ -8,9 +8,6 @@
 @section('content')
 <div class="container-block">
     <div class="wrap text-center">
-        <div class="logo-img">
-            <img src="<?php echo asset('/svg/New LOGO.svg');?>" width="300px" alt="" class="img-fluid">
-        </div>
         <div class="page-title">會員資訊</div>
         <form class="outer">
             <div class="subTitle">個人資訊</div>
@@ -30,9 +27,9 @@
             </div>
             <div class="input-area">
                 <div class="input-text">*連絡電話：</div>
-                <input type="text" value="{{$data['telephone_number'] }}" id="telephone_number">
+                <input type="text" value="{{$data['telephone_number'] }}" id="telephone_number" placeholder="ex：048720552" onkeyup='this.value=this.value.replace(/\D/gi,"")'>
                 <div class="input-text">*行動電話：</div>
-                <input type="text" value="{{$data['phone_number'] }}" id="phone_number">
+                <input type="text" value="{{$data['phone_number'] }}" id="phone_number" placeholder="ex: 0958213456" onkeyup='this.value=this.value.replace(/\D/gi,"")'>
             </div>
             <div class="input-area">
                 <div class="input-text">*郵遞區號：</div>
--- LOHO_Web/resources/views/Account/ViewAllOrderHistory.blade.php
+++ LOHO_Web/resources/views/Account/ViewAllOrderHistory.blade.php
@@ -5,35 +5,50 @@
 <link href="{{ URL::asset('/css/ForgetPasswordToModify.css') }}" rel="stylesheet" type="text/css" />
 @stop
 @section('content')
-<div class = "content d-flex flex-column align-items-center">
-    <div>
-        <img src="<?php echo asset('/svg/New LOGO.svg');?>" width="300px" alt="" class="img-fluid">
-    </div>
+<div class = "container-block d-flex flex-column align-items-center text-JhengHei ">
     <p class="h1">我的訂單紀錄</p>
       <table class="table">
           <thead class="thead-dark">
             <tr>
-              <th scope="col">訂單編號</th>  
-              <th scope="col">商品金額</th>
+              <th scope="col">訂單編號</th>
+              <th scope="col">購買日期</th>
+              <th scope="col">訂單狀態</th>   
+              <th scope="col">訂單小計</th>
               <th scope="col">運費小計</th>
               <th scope="col">優惠折抵</th>
-              <th scope="col">優惠代碼</th>
-              <th scope="col">總計</th>
+              <th scope="col">總額</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody class='table-collapse'>
               @foreach ($order_historys_list as $order_history)
               <tr>
-                    <td class="order_id">{{$order_history['id']}}</td>
+                  <td class="order_id">{{$order_history['id']}}</td>
                   <td>{{$order_history['goodsTotal']}}</td>
                   <td>{{$order_history['shippingFee']}}</td>
                   <td>{{$order_history['coupon_price']}}</td>
                   <td>{{$order_history['coupon_code']}}</td>
                   <td>{{$order_history['orderTotal']}}<button class = "detail_order_info">詳細資訊</button></td>
-              </tr>             
-              @endforeach     
+              </tr>          
+              @endforeach   
+            <!--<tr>
+                <td><a href="">#S0026519845</a></td>
+                <td>2017/5/20</td>
+                <td>已完成</td>
+                <td>$560</td>   
+                <td>$70(宅配到府)</td>
+                <td>100</td>
+                <td>$530</td>
+            </tr>-->
           </tbody>
         </table>
+        @if(sizeof($order_historys_list) == 0) 
+            <div>
+                <div>
+                    <p class='text-danger font-weight-bold'>目前無商品資料</p>
+                </div>
+                <button type="button" class='btn btn-outline-custom' onclick="javascript:location.href='{{ url("Shopping/BrowseItems")}}'" id="logout_btn">現在去逛逛</button>
+            </div>
+        @endif
 </div>   
 <script>
 $('.detail_order_info').click(function (e) { 
